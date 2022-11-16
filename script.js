09f1167ff5a4c3b8f515d6af9fc94fcf4078cefb
GeorgeMc1 <george.man1@hotmail.co.uk>
2022-11-16 15:00:37 +0000

    added happy to all

--- script.js
+++ script.js
@@ -5,24 +5,33 @@ class pet {
         this.hunger = 100;
         this.thirst = 100;
         this.energy = 100;
+        this.happy = 100;
     }
     eat() {
         this.hunger += 20;
+        this.happy += 10;
+        return this;
     }
     drink() {
         this.thirst += 20;
+        this.happy += 10;
+        return this;
     }
     sleep() {
         this.energy -= 20;
+        return this;
     }
     cycle() {
         this.hunger -= 1;
         this.thirst -= 1;
         this.energy -= 1;
-        this.cycle();
+        this.happy -= 1;
+        setTimeout(() => {
+            this.cycle();
+        }, 200);
+        return this;
     }
 }
-
 class dog extends pet {
     constructor(name) {
         super(name);
@@ -32,13 +41,13 @@ class dog extends pet {
         this.health += 10;
         this.thirst -= 10;
         this.energy -= 10;
+        this.happy += 25;
+        return this;
     }
 }
-
 class cat extends pet {
-    constructor(name, happy) {
-        super(name, happy);
-        this.happy = 100;
+    constructor(name) {
+        super(name);
     }
     play() {
         this.hunger -= 10;
@@ -46,6 +55,7 @@ class cat extends pet {
         this.thirst -= 10;
         this.energy -= 10;
         this.happy += 25;
+        return this;
     }
 }
 class guineaPig extends pet {
@@ -57,6 +67,7 @@ class guineaPig extends pet {
         this.hunger -= 10;
         this.energy -= 20;
         this.thirst -= 20;
+        this.happy += 10;
         // console.log(`${this.name} is happy`);
         return this;
     }
@@ -64,6 +75,7 @@ class guineaPig extends pet {
         this.health += 10;
         this.hunger -= 30;
         this.thirst -= 10;
+        this.happy += 10;
         // console.log(`${this.name} is relieved.`);
         return this;
     }
