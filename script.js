dad643255532d422364c6b0c874e7b6f1743992f
GeorgeMc1 <george.man1@hotmail.co.uk>
2022-11-17 12:07:39 +0000

    start of modal

--- script.js
+++ script.js
@@ -8,28 +8,41 @@ class pet {
         this.happy = 100;
     }
     eat() {
+        this.health += 10;
         this.hunger += 20;
-        this.happy += 10;
+        this.happy += 5;
+        this.energy += 5;
         return this;
     }
     drink() {
+        this.health += 5;
         this.thirst += 20;
-        this.happy += 10;
+        this.happy += 5;
+        this.energy += 5;
         return this;
     }
     sleep() {
-        this.energy -= 20;
+        this.energy += 20;
         return this;
     }
     cycle() {
+        this.health -= 1;
         this.hunger -= 1;
         this.thirst -= 1;
         this.energy -= 1;
         this.happy -= 1;
+        this.updateBars();
         setTimeout(() => {
             this.cycle();
         }, 200);
         return this;
+    }
+    updateBars(){
+        health.value = this.health;
+        hunger.value = this.hunger;
+        thirst.value = this.thirst;
+        energy.value = this.energy;
+        happy.value = this.happy;
     }
 }
 class dog extends pet {
@@ -80,3 +93,29 @@ class guineaPig extends pet {
         return this;
     }
 }
+dogBtn.addEventListener("click", () => {
+    let name = prompt("What would you like to call your dog?", "bill");
+    let currentPet = new dog(name);
+    currentPet.cycle();
+})
+walk.addEventListener("click", () => {
+    currentPet.walk();
+})
+eat.addEventListener("click", () => {
+    currentPet.eat();
+})
+drink.addEventListener("click", () => {
+    currentPet.drink();
+})
+sleep.addEventListener("click", () => {
+    currentPet.sleep();
+})
+runWheel.addEventListener("click", () => {
+    currentPet.runWheel();
+})
+play.addEventListener("click", () => {
+    currentPet.play();
+})
+poops.addEventListener("click", () => {
+    currentPet.poops();
+})
